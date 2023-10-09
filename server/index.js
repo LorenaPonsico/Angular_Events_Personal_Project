6c326295a19bdf7a51e0ab943f887596842d9676
Lorena Ponsico <lorena.poca@gmail.com>
2023-10-09 16:40:50 +0200

    index.js port

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const cors = require('cors');
 
 //creamos el servidor
 const app = express();
+const port = process.env.PORT || 4000;
 
 //conectamos a la BD
-conectarDB();
+// conectarDB();
 
 // middleware
 app.use(cors());
@@ -14,6 +15,17 @@ app.use(express.json()) // para poder enviar json a nuestra app
 app.use('/api/events', require('./routes/events.routes'));
 app.use('/api/users', require('./routes/users.routes'));
 
-app.listen(4000, () => {
-    console.log('server is working')
-})
+
+const startServer = () => {
+    try{
+    //conectar con DB
+    conectarDB();
+    // start listen
+app.listen(port, () => console.log(`server is working on ${port}`));
+
+} catch(error) {
+    console.log(error)
+}
+};
+
+startServer()
